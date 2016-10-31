d2a5b6fdc3029b954a2556b7551f97fb4465433c
sofieriisberg <sofieriisberg95@hotmail.com>
2016-10-31 19:25:38 +0100

    ValidateCPR
    Metoden er opdateret med tidligere CPR-checker metode

--- Logic_blodtryksmåler/Gem.cs
+++ Logic_blodtryksmåler/Gem.cs
@@ -9,10 +9,24 @@ namespace Logic_blodtryksmåler
     public class Gem
     {
         private Dataaccess_blodtryksmåler.GemPatient GemPatient;
+        private int[] weightCPR = { 4, 3, 2, 7, 6, 5, 4, 3, 2, 1 };
 
-        public bool ValidateCPR(int cpr)
+        public bool ValidateCPR(String cpr)
         {
-            throw new System.NotImplementedException();
+            int sum = 0;
+            if (cpr.Length == 10)
+            {
+                for (int i = 0; i < cpr.Length; i++)
+                {
+                    char[] chars = cpr.ToCharArray();
+                    sum += (chars[i] - 48) * weightCPR[i]; //de 48 trækkes fra for at lave char om til int-værdier
+                }
+
+                int res = sum % 11;
+                if (res == 0)
+                    return true;
+            }
+            return false;
         }
 
         public void SaveBT(DTO_login login, DTO_data data)
